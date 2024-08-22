9a17abd1b5aab74937ff43751e78da6d610c991a
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2024-08-22 00:03:38 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import { useState } from 'react';
 function App() {
 
   const [number, setNumber] = useState(0);
+  const [dato, setDato] = useState("");
+  const [dateFact, setDateFact] = useState("");
+  const [yearFact, setYearFact] = useState("");
   const [date, setDate] = useState({
     day: 0,
     month: 0,
     year: 0
   })
 
+  const getNumber = () => {
+    fetch(`http://numbersapi.com/${number}/math?json`)
+    .then((response)=> response.json())
+    .then((data)=>  setDato(data.text))
+    .catch((error)=> console.log(error))
+  }
+  const getDate = () => {
+    
+    fetch(`http://numbersapi.com/${date.day}/${date.month}/date?json`)
+    .then((response)=> response.json())
+    .then((data)=>  setDateFact(data.text))
+    .catch((error)=> console.log(error))
+
+    fetch(`http://numbersapi.com/${date.year}/year?json`)
+    .then((response)=> response.json())
+    .then((data)=>  setYearFact(data.text))
+  }
   const handleNumberChange = (event) => setNumber(event.target.value)
 
   const handleDateChange = (event) => {
@@ -31,18 +51,18 @@ function App() {
       <section>
         <h2>Obtener un dato sobre un número</h2>
         <input type="number" onChange={handleNumberChange} />
-        <button>Enviar</button>
-        <p className="result-box"></p>
+        <button onClick={getNumber}>Enviar</button>
+        <p className="result-box">{dato}</p>
       </section>
 
       <section>
         <h2>Obtener un dato sobre una fecha</h2>
         <input type="date" onChange={handleDateChange} />
-        <button>Enviar</button>
+        <button onClick={getDate}>Enviar</button>
         <h3>Dato sobre la fecha</h3>
-        <p className="result-box"></p>
+        <p className="result-box">{dateFact}</p>
         <h3>Dato sobre el año</h3>
-        <p className="result-box"></p>
+        <p className="result-box">{yearFact}</p>
       </section>
 
     </main>
